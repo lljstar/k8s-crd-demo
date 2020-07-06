78d6f1df5bdb8b705a459bf136abd9b130a41cf0
刘立军 <lljstar@163.com>
2020-07-07 03:12:45 +0800

    add

---
+++ go.mod
@@ -0,0 +1,5 @@
+module k8s-crd-demo
+
+go 1.14
+
+require k8s.io/apimachinery v0.18.5
---
+++ pkg/apis/samplecrd/register.go
@@ -0,0 +1,8 @@
+package samplecrd
+
+const (
+	// GroupName apis/{group}
+	GroupName = "samplecrd.k8s.io"
+	// Version apis/{group}/v1
+	Version = "v1"
+)
